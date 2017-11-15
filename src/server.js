7e31298575623d1fbe5430d5202c5e513fda463e
hamzicabdulah <abdulahhamzic@gmail.com>
2017-11-15 15:02:31 +0000

    Added to server file

--- src/server.js
+++ src/server.js
@@ -1,9 +1,21 @@
 const express = require('express');
 const path = require('path');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const routes = require('./routes');
 require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.MONGO_URI, {
+    useMongoClient: true
+});
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.use(express.static(path.resolve(__dirname, 'public')));
 routes(app);
